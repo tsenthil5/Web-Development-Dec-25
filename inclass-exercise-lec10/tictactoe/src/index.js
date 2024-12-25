498b571a240c75c7d93f0652d5e522e9e9eebba9
senthil1999 <st108@rice.edu>
2024-12-25 13:53:41 -0600

    Add package-lock.json and console.log message in index.js

--- inclass-exercise-lec10/tictactoe/src/index.js
+++ inclass-exercise-lec10/tictactoe/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+console.log("test message");
 
 /**
  * Tic-tac-toe square.
